ade63016ea62dfec5823bc7b8d8f0b3759640ce7
MelihInceoglu <av.melihinceoglu@gmail.com>
2023-07-04 16:48:49 +0300

    first commit

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,6 +1,8 @@
 package utilities;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.Set;
 
@@ -15,7 +17,6 @@ public class ReusableMethods {
 
        Set<String> windowHandleSeti =driver.getWindowHandles();
 
-
         for (String each: windowHandleSeti) {
             if (!each.equals(ilksayfaWindowHandleDegeri)){
                 ikinciSayfaWHD=each;
@@ -24,19 +25,17 @@ public class ReusableMethods {
 
         }
 
-
-
-
-
-
-
-
         return ikinciSayfaWHD;
-
-
-
-
     }
 
+
+
+
+    public static String hucredekiYaziyiGetir(int satirNo , int sutunNo, WebDriver driver){
+        //               //tbody/tr[7]/td[1]
+        String dinamikXPath = "//tbody/tr["+satirNo+"]/td["+sutunNo+"]";
+        WebElement istenenHucredekiElement = driver.findElement(By.xpath(dinamikXPath));
+        return istenenHucredekiElement.getText();
+    }
 
 }
